250e4b828dd8987b1cd6d4aef075d861d50a5b82
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-01-03 16:55:13 +1100

    20240103050209_users

---
+++ supabase/migrations/20240103050209_users.sql
@@ -0,0 +1,112 @@
+revoke delete on table "public"."users" from "anon";
+
+revoke insert on table "public"."users" from "anon";
+
+revoke references on table "public"."users" from "anon";
+
+revoke select on table "public"."users" from "anon";
+
+revoke trigger on table "public"."users" from "anon";
+
+revoke truncate on table "public"."users" from "anon";
+
+revoke update on table "public"."users" from "anon";
+
+revoke delete on table "public"."users" from "authenticated";
+
+revoke insert on table "public"."users" from "authenticated";
+
+revoke references on table "public"."users" from "authenticated";
+
+revoke select on table "public"."users" from "authenticated";
+
+revoke trigger on table "public"."users" from "authenticated";
+
+revoke truncate on table "public"."users" from "authenticated";
+
+revoke update on table "public"."users" from "authenticated";
+
+revoke delete on table "public"."users" from "service_role";
+
+revoke insert on table "public"."users" from "service_role";
+
+revoke references on table "public"."users" from "service_role";
+
+revoke select on table "public"."users" from "service_role";
+
+revoke trigger on table "public"."users" from "service_role";
+
+revoke truncate on table "public"."users" from "service_role";
+
+revoke update on table "public"."users" from "service_role";
+
+alter table "public"."users" drop constraint "users_id_fkey";
+
+alter table "public"."users" drop constraint "users_pkey";
+
+drop index if exists "public"."users_pkey";
+
+drop table "public"."users";
+
+create table "public"."profiles" (
+    "id" uuid not null,
+    "first_name" text,
+    "last_name" text,
+    "created_at" timestamp with time zone default now(),
+    "updated_at" timestamp with time zone default now()
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX users_pkey ON public.profiles USING btree (id);
+
+alter table "public"."profiles" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+
