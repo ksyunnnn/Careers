1b66fa49174d2fc15f13ec16243d5e9438fcabf0
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-01-15 15:29:01 +1100

    set setting-bar

--- src/components/SidebarInSettings.tsx
+++ src/components/SidebarInSettings.tsx
@@ -1,35 +1,56 @@
+'use client';
+
 import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import { ScrollArea } from '@/components/ui/scroll-area';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+import { Route } from 'next';
+import { usePathname } from 'next/navigation';
 
 type Props = {
   className?: string;
 };
 
+const items: {
+  href: Route;
+  title: string;
+}[] = [
+  {
+    title: 'Account',
+    href: '/settings/account',
+  },
+  {
+    title: 'Appearance',
+    href: '/',
+  },
+  {
+    title: 'Notifications',
+    href: '/',
+  },
+];
+
 export const SidebarInSettings = ({ className }: Props) => {
+  const pathname = usePathname();
   return (
-    <ScrollArea className={cn('pb-12 h-content-min-height', className)}>
-      <div className="space-y-4 py-4">
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Account</h2>
-          <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start">
-              2023.12
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              2023.11
-            </Button>
-          </div>
-        </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">View</h2>
-          <div className="space-y-1">
-            <Button variant="ghost" className="w-full justify-start">
-              Timeline
-            </Button>
-          </div>
-        </div>
-      </div>
-    </ScrollArea>
+    <div className={cn('pb-12 h-content-min-height', className)}>
+      <aside className="lg:w-1/5">
+        <nav className="flex">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href as Route}
+              className={cn(
+                buttonVariants({ variant: 'ghost' }),
+                pathname === item.href
+                  ? 'bg-muted hover:bg-muted'
+                  : 'hover:bg-transparent hover:underline',
+                'justify-start'
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      </aside>
+    </div>
   );
 };
