0d980a2de732f5d6dbded718000d83bb100061cc
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-01-27 16:33:24 +1100

    change triger

---
+++ supabase/migrations/20240127052918_replace_trigger_handle_new_user.sql
@@ -0,0 +1,13 @@
+create or replace function public.handle_new_user() 
+returns trigger as $$
+begin
+  insert into public.profiles (id, email, user_name)
+  values (new.id, new.email, new.raw_user_meta_data ->> 'first_name');
+  return new;
+end;
+$$ language plpgsql security definer;
+
+-- trigger the function every time a user is created
+create or replace trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
