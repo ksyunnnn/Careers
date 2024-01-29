4520e70597800ba3b055a7915827bd9d749d7f08
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-01-29 16:17:33 +1100

    add pallarel

---
+++ src/app/@modal/(wizard)/(.)[careerId]/edit/page.tsx
@@ -0,0 +1,20 @@
+import { EditOrNewCareerForm } from '@/components/EditOrNewCareerForm';
+import { Modal } from '@/components/Modal';
+
+type Context = {
+  params: {
+    careerId: string;
+  };
+};
+
+const Page = ({ params }: Context) => {
+  return (
+    <Modal>
+      <div className="h-[92vh]">
+        <EditOrNewCareerForm parallel="modal" careerId={params.careerId} />
+      </div>
+    </Modal>
+  );
+};
+
+export default Page;
